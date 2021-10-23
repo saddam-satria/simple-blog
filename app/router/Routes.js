5d7b1b76a7d2a1f3b8e6cce66ee2af7f1998f498
saddam-satria <ardhisaddam@gmail.com>
2021-10-23 09:48:29 +0700

    Update: Routing middleware auth

--- app/router/Routes.js
+++ app/router/Routes.js
@@ -27,18 +27,20 @@ const uploadImagePost = multer({ storage: optionPostImage.storage, fileFilter: o
 // Api
 
 // users
+// Admin do
 router.route('/api/v1/users').post(rateLimiter, userControllers.addUser).get(userControllers.getAllUsers);
 router.route('/api/v1/user/:id').delete(userControllers.deleteUser).put(uploadImageUser.single('image'), userControllers.updateUser).get(userControllers.detailUser);
+// Not need this up
+
 router.post('/api/v1/user/login', rateLimiter, userControllers.loginUser);
-router.get('/api/v1/revoketoken', userControllers.revokeAccessToken);
+router.get('/api/v1/revoketoken', authMiddleware.auth, userControllers.revokeAccessToken);
 
 // Author
-router.route('/api/v1/authors').post(authMiddleware.auth, authorsController.addAuthor).get(authMiddleware.auth, authorsController.getAuthors);
+router.route('/api/v1/authors').post(authMiddleware.auth, authorsController.addAuthor).get(authorsController.getAuthors);
 router.route('/api/v1/author/:id').delete(authMiddleware.auth, authorsController.deleteAuthor).put(authMiddleware.auth, authorsController.updateAuthors).get(authMiddleware.auth, authorsController.getDetailAuthor);
 
 // Posts
-router.route('/api/v1/posts').post(uploadImagePost.single('image'), postsController.addPost);
-router.get('/api/v1/posts/:limit', authMiddleware.auth, postsController.getPosts);
-router.route('/api/v1/post/:id').delete(postsController.deletePost).put(postsController.updatePost).get(postsController.detailPost);
+router.route('/api/v1/posts').post(authMiddleware.auth, uploadImagePost.single('image'), postsController.addPost).get(postsController.getPosts);
+router.route('/api/v1/post/:id').delete(authMiddleware.auth, postsController.deletePost).put(authMiddleware.auth, postsController.updatePost).get(postsController.detailPost);
 
 module.exports = router;
