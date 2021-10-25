8782acf4c465dcd1fd28678fe0b611402ebce45f
saddam-satria <ardhisaddam@gmail.com>
2021-10-25 14:37:38 +0700

    Day 2 add: Navbar and card components

---
+++ app/views/src/components/CardComponent.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { BASE_URL_IMAGE } from '../api/config';
+
+const CardComponent = ({ blog }) => {
+  const desc = blog.desc.slice(0, 100);
+
+  return (
+    <div className="Card">
+      <div className="w-full h-full flex justify-center">
+        <div className="bg-white w-12/12 rounded-md">
+          <div className="card-image w-full">
+            <img className="object-cover w-full h-80 rounded-t-md" src={ BASE_URL_IMAGE + "blogs/" + blog.image} alt="card component" srcset="" />
+          </div>
+          <div className="card-body py-4">
+            <h1 className="font-poppins capitalize font-medium text-2xl">{blog.title}</h1>
+            <div className="flex py-2">
+              <h3 className="font-poppins capitalize font-medium text-lg">{blog.author}</h3>
+              <div className="ml-auto">
+                <span>{blog.create}</span>
+              </div>
+            </div>
+            <p className="text-justify font-poppins text-lg font-normal ">
+              {desc}{' '}
+              <Link to={'#'} className="text-gray-500 font-poppins font-normal">
+                read more ...
+              </Link>{' '}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardComponent;
---
+++ app/views/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const Navbar = () => {
+  const [menus] = useState([
+    {
+      name: 'home',
+      link: '/',
+    },
+    {
+      name: 'writer',
+      link: '/about',
+    },
+  ]);
+
+  const location = useLocation();
+  return (
+    <div className="Navbar bg-transparent w-full absolute top-0 z-40">
+      <div className="navbar nav mx-12  py-4 sm:mx-18 lg:mx-32">
+        <div className="flex py-0 sm:py-2">
+          <h1 className={`font-poppins font-bold ${location.pathname === '/' ? 'hover:text-blue-50 text-white' : 'text-gray-600 hover:text-gray-500'} `}>
+            <Link to={'/'}>PERN STACK</Link>
+          </h1>
+          <div className="ml-auto sm:hidden capitalize">asdsad</div>
+          <div className="ml-auto sm:w-40 right-full sm:right-0  absolute sm:relative z-40 bg-black opacity-80 sm:bg-transparent top-0 px-24 sm:px-0 h-screen sm:h-auto">
+            <ul className=" flex sm:flex-row flex-col h-80 pt-24 sm:h-auto sm:pt-0 justify-around">
+              {menus.map((menu, index) => {
+                return (
+                  <li key={index} className={`font-poppins font-medium capitalize ${location.pathname === '/' ? 'hover:text-blue-50 text-white' : 'text-gray-600 hover:text-gray-500'}`}>
+                    <Link to={menu.link}>{menu.name}</Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
