d17d55e39af364c81960eba6fcc579df3b609770
saddam-satria <ardhisaddam@gmail.com>
2021-10-25 14:39:17 +0700

    Day 2 add: global tailwindcss setting app js router

--- app/views/src/App.js
+++ app/views/src/App.js
@@ -1,9 +1,24 @@
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home, Blogs, Login, Register, Authors, Notfound, DetailBlog } from './pages';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path={'/'} component={Home} exact />
+          <Route path={'/blogs'} component={Blogs} />
+          <Route path={'/blog/:id'} component={DetailBlog} />
+          <Route path={'*'} component={Notfound} />
+
+          {/* Protected Route */}
+          <Route path={'/authors'} component={Authors} />
+          <Route path={'/login'} component={Login} />
+          <Route path={'/register'} component={Register} />
+        </Switch>
+      </Router>
     </div>
   );
 }
